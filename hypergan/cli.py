64cb4d90b998548a08931384d2f3638b6cf09bff
shantih19 <shantih19@gmail.com>
2020-01-23 17:39:22 +0100

    scream

--- hypergan/cli.py
+++ hypergan/cli.py
@@ -216,7 +216,7 @@ class CLI:
             features = tf.parse_example(serialized_tf_example, spec)
             return tf.estimator.export.ServingInputReceiver(features, receiver_tensors)
 
-        self.gan = self.gan_fn(self.gan_config, self.inputs_fn(self.args.batch_size))
+        self.gan = self.gan_fn(self.gan_config, self.inputs_fn(self.args.batch_size), reuse=tf.AUTO_REUSE)
         est = self.gan.tpu_gan_estimator(config)
 
         config = tf.ConfigProto()
